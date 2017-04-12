ea82da976da7bd03883c79e26823f9ea2dcd3565
Ben Gaudiosi <bgaudiosi531@gmail.com>
2017-04-11 20:50:40 -0400

    can save to db

--- app.js
+++ app.js
@@ -4,6 +4,54 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/foodforme')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+        console.log("Successfully connected to MongoDB with Mongoose");
+        });
+
+var restaurant_schema =  new Schema({
+                                    "apiKey": String,
+                                    "deliveryMin": Number,
+                                    "logoUrl": String,
+                                    "name": String,
+                                    "streetAddress": String,
+                                    "city": String,
+                                    "state": String,
+                                    "zip": String,
+                                    "foodTypes": Array,
+                                    "phone": String,
+                                    "latitude": Number,
+                                    "longitude": Number,
+                                    "minFreeDelivery": Number,
+                                    "taxRate": Number,
+                                    "acceptsCash": Boolean,
+                                    "acceptsCard": Boolean,
+                                    "offersPickup": Boolean,
+                                    "offersDelivery": Boolean,
+                                    "isTestRestaurant": Boolean,
+                                    "minWaitTime": Number,
+                                    "maxWaitTime": Number,
+                                    "open": Boolean,
+                                    "url": String,
+                                    "hours": {
+                                    "Sunday": Array,
+                                    "Monday": Array,
+                                    "Tuesday": Array,
+                                    "Wednesday": Array,
+                                    "Thursday": Array,
+                                    "Friday": Array,
+                                    "Saturday": Array
+                                    },
+                                    "timezone": String
+                                    });
+
+var Restaurant = mongoose.model('Restaurant', restaurant_schema);
 var mongo = require('mongodb');
 
 var index = require('./routes/index');
@@ -26,57 +74,69 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Post method for searches
 app.post('/', function(req, res) {
-	var search_val = req.body.search;
-	var location_val = req.body.loc;
-	
-	var http = require("https");
-	var options = {
-		"method": "GET",
-		"hostname": "api.eatstreet.com",
-		"port": null,
-		"path": "/publicapi/v1/restaurant/search?street-address=ADDRESS&search=SEARCH_VAL",
-		"headers": {
-			"x-access-token": "563aa311f08441be",
-			"cache-control": "no-cache",
-		 	"postman-token": "7e22b582-35fe-164b-d5f7-10c1860cd158"
-		}
-	}
-	options.path = options.path.replace("ADDRESS", location_val);
-	options.path = options.path.replace("SEARCH_VAL", search_val);
-	
-	var api_req = http.request(options, function (api_res) {
-		var chunks = [];
-		api_res.on("data", function (chunk) {
-			chunks.push(chunk);
-		});
-		api_res.on("end", function () {
-			var body = Buffer.concat(chunks);
-			res.send(JSON.parse(body.toString()));
-			res.end();
-		});
-	});
-	api_req.end();
-});
+         var search_val = req.body.search;
+         var location_val = req.body.loc;
+         
+         var http = require("https");
+         var options = {
+         "method": "GET",
+         "hostname": "api.eatstreet.com",
+         "port": null,
+         "path": "/publicapi/v1/restaurant/search?street-address=ADDRESS&search=SEARCH_VAL",
+         "headers": {
+         "x-access-token": "563aa311f08441be",
+         "cache-control": "no-cache",
+         "postman-token": "7e22b582-35fe-164b-d5f7-10c1860cd158"
+         }
+         }
+         options.path = options.path.replace("ADDRESS", location_val);
+         options.path = options.path.replace("SEARCH_VAL", search_val);
+         
+         var api_req = http.request(options, function (api_res) {
+                                    var chunks = [];
+                                    api_res.on("data", function (chunk) {
+                                               chunks.push(chunk);
+                                               });
+                                    api_res.on("end", function () {
+                                               var body = Buffer.concat(chunks);
+                                               var data = JSON.parse(body.toString());
+                                               var restaurants = data.restaurants;
+                                               res.send(data);
+                                               for (var i = 0; i < restaurants.length; i++) {
+                                               var curr_restaurant = new Restaurant(restaurants[i])
+                                               curr_restaurant.save(function (err, curr_restaurant) {
+                                                                    if (err) {
+                                                                    console.log("Error saving to DB");
+                                                                    return console.error(err);
+                                                                    }
+                                                                    //console.log("Saving " + restaurants[i].name + " to DB.");
+                                                                    });
+                                               }
+                                               res.end();
+                                               });
+                                    });
+         api_req.end();
+         });
 //app.use('/auth', authenticate);
 app.use('/', index);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+        });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+        });
 
 module.exports = app;
